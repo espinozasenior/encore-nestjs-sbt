457c83f57e5ec655a4c90337f814e5cc01fd2901
Espinozasenior <lespinoza.developer@gmail.com>
2024-07-19 22:36:37 -0500

    merging with nestjs squareboat version
    https://squareboat.com/open-source/nestjs-boilerplate/

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { BoatModule } from '@libs/boat';
+import { ObjectionModule, DatabaseOptions } from '@squareboat/nestjs-objection';
+import { LocalizationModule } from '@squareboat/nestjs-localization';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ObjectionModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        return config.get('db');
+      },
+      inject: [ConfigService],
+    }),
+    LocalizationModule.register({
+      path: 'resources/lang',
+      fallbackLang: 'en',
+    }),
+    BoatModule,
+    CatsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
