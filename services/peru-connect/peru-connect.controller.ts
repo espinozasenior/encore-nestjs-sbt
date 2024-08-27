bedd36ea4d5b7d6eb2bd32694fa54ed986d7dbf4
Luis Quiñones <lpaandres2020@gmail.com>
2024-08-27 14:23:22 -0600

    feat(peru-connect): endpoint to search by ruc

--- services/peru-connect/peru-connect.controller.ts
+++ services/peru-connect/peru-connect.controller.ts
@@ -2,6 +2,7 @@ import { api, APIError } from "encore.dev/api";
 
 import applicationContext from "../applicationContext";
 import type { DniDto } from "./dtos/dni.dto";
+import type { RucDto } from "./dtos/ruc.dto";
 
 export const searchByDNI = api(
   { expose: true, method: "GET", path: "/peru-connect/search-by-dni/:dni" },
@@ -11,6 +12,20 @@ export const searchByDNI = api(
     const result = await peruConnectService.searchByDNI(dni);
     if (!result) {
       throw APIError.notFound("dni not found");
+    }
+
+    return result;
+  },
+);
+
+export const searchByRUC = api(
+  { expose: true, method: "GET", path: "/peru-connect/search-by-ruc/:ruc" },
+  async ({ ruc }: { ruc: string }): Promise<RucDto> => {
+    const { peruConnectService } = await applicationContext;
+
+    const result = await peruConnectService.searchByRUC(ruc);
+    if (!result) {
+      throw APIError.notFound("ruc not found");
     }
 
     return result;
