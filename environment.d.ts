eb88dfc603ac952abb49299b663f7d8f32f86b2c
Luis Quiñones <lpaandres2020@gmail.com>
2024-09-02 14:41:03 -0600

    chore: removed old environment variables types from ts declaration file

--- environment.d.ts
+++ environment.d.ts
@@ -1,13 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      CLERK_SECRET_KEY: string;
-      CLERK_PUBLISHABLE_KEY: string;
-      CLERK_JWT_PUBLIC_KEY: string;
-      INTERNAL_SUNAT_ENCRYPTION_KEY: string;
       NODE_ENV: "development" | "production";
-      APIPERU_URL: string;
-      APIPERU_API_KEY: string;
     }
   }
 }
