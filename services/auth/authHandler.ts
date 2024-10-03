0bf925fb0e3635b5e160539b0ab80b88e1880d32
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-03 08:49:20 -0600

    feat(auth): added error boundary for authentication middleware

--- services/auth/authHandler.ts
+++ services/auth/authHandler.ts
@@ -1,4 +1,4 @@
-import { type Header, Gateway } from "encore.dev/api";
+import { type Header, APIError, Gateway } from "encore.dev/api";
 import { authHandler } from "encore.dev/auth";
 
 import type { AuthenticatedUser } from "./interfaces/clerk.interface";
@@ -10,17 +10,29 @@ interface AuthParams {
 
 export const auth = authHandler<AuthParams, AuthenticatedUser>(
   async (params): Promise<AuthenticatedUser> => {
-    const { authService } = await applicationContext;
+    try {
+      const { authService } = await applicationContext;
 
-    const clerkToken = params.authorization.replace("Bearer ", "");
-    const jwt = await authService.verifyToken(clerkToken);
+      const clerkToken = params.authorization.replace("Bearer ", "");
+      const jwt = await authService.verifyToken(clerkToken);
 
-    const clerkUser = await authService.getUser(jwt.sub);
+      const clerkUser = await authService.getUser(jwt.sub);
 
-    return {
-      userID: jwt.sub,
-      metadata: clerkUser,
-    };
+      return {
+        userID: jwt.sub,
+        metadata: clerkUser,
+      };
+    } catch (error) {
+      if (error instanceof APIError) {
+        throw error;
+      }
+
+      console.error(
+        `something went unexpectedly wrong while authenticating user: ${JSON.stringify(error)}`,
+      );
+
+      throw APIError.deadlineExceeded("something went wrong");
+    }
   },
 );
 
